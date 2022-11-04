ac611c38fced7b215e834b23219a5cb5e51d2c9d
Sérgio <a18074@alunos.ipca.pt>
2022-11-04 18:21:53 +0000

    Work Done: Configuration the application to use Slide Material. Creating sliding intro on activity.
    * Notes:
     - ...
    
    * Add items:
    - Adding images
    - adding Intros fragments
    
    * Updated items:
    - MainActivity
    
    * Removed:
    ...

--- app/src/main/java/udemy/java/organizze/MainActivity.java
+++ app/src/main/java/udemy/java/organizze/MainActivity.java
@@ -4,11 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+
+        /*
+        *
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descrição")
+                .image(R.drawable.um)
+                .background(R.color.purple_200)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descrição")
+                .image(R.drawable.dois)
+                .background(R.color.purple_500)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descrição")
+                .image(R.drawable.tres)
+                .background(R.color.purple_700)
+                .build()
+        );
+         */
     }
 }
