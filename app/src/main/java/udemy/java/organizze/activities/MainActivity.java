c526b9182f46b349391cec33fea4ea8f7047cf81
Sérgio <a18074@alunos.ipca.pt>
2022-11-15 01:05:56 +0000

    Work Done: Adding material design to the project.
    * Notes:
     - ...
    
    * Add items:
    - Fragments
    - MainActivity
    - layout's
    - ic_add
    
    * Updated items:
    - MainActivity
    - colors
    - AndroidManifest
    - layout's
    
    * Removed:
    ...

--- app/src/main/java/udemy/java/organizze/activities/MainActivity.java
+++ app/src/main/java/udemy/java/organizze/activities/MainActivity.java
@@ -1,22 +1,67 @@
 package udemy.java.organizze.activities;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import udemy.java.organizze.databinding.ActivityMainBinding;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import udemy.java.organizze.R;
+import udemy.java.organizze.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    private FloatingActionButton addExpenses, addReceived;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        setContentView(binding.getRoot());
 
+        setSupportActionBar(binding.toolbar);
+
+        addExpenses = binding.fabMenuExpense;
+        addReceived = binding.fabMenuReceived;
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fabMenuExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        binding.fabMenuReceived.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
