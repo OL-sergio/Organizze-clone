2cf4bdabed53fbe7cf991d9f316067fc828e5964
Sérgio <a18074@alunos.ipca.pt>
2022-11-23 16:48:27 +0000

    Work Done: Adding model movements to project and saving the values on firebase.
    * Notes:
     - ...
    
    * Add items:
    - Movements
    
    * Updated items:
    - DateCustom
    - ExpenseActivity
    
    * Removed:
    ...

--- app/src/main/java/udemy/java/organizze/activities/ExpenseActivity.java
+++ app/src/main/java/udemy/java/organizze/activities/ExpenseActivity.java
@@ -1,14 +1,17 @@
 package udemy.java.organizze.activities;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import udemy.java.organizze.databinding.ActivityExpenseBinding;
 import udemy.java.organizze.helper.DateCustom;
+import udemy.java.organizze.model.Movements;
 
 public class ExpenseActivity extends AppCompatActivity {
 
@@ -16,6 +19,8 @@ public class ExpenseActivity extends AppCompatActivity {
 
     private TextInputEditText valueDate, valueCategory, valueDescription;
     private EditText imputValue;
+    private FloatingActionButton fabExpense;
+    private Movements movements;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,28 @@ public class ExpenseActivity extends AppCompatActivity {
         valueCategory = binding.editTextExpenseCategory;
         valueDescription = binding.editTextExpenseDescription;
         imputValue = binding.editTexExpense;
+        fabExpense = binding.fabExpenseDone;
 
         valueDate.setText(DateCustom.actualDate());
 
-
+        fabExpense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  saveExpense() ;  
+            }
+        });
     }
 
+    private void saveExpense() {
+
+        String date = valueDate.getText().toString();
+        movements = new Movements();
+        movements.setValue( Double.parseDouble(imputValue.getText().toString()) );
+        movements.setCategory(valueCategory.getText().toString() );
+        movements.setDescription(valueDescription.getText().toString() );
+        movements.setData(valueDate.getText().toString());
+        movements.setType("expense");
+
+        movements.save(date);
+    }
 }
