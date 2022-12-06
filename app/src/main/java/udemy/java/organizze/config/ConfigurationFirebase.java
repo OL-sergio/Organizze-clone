f19e152fd3a3d456e47a597d98e521bf70f8ce1f
Sérgio <a18074@alunos.ipca.pt>
2022-12-06 17:58:25 +0000

    Work Done: Retrieving value from Firebase, then presenting user and calculated the total balances;
    * Notes:
     - ..
    
    * Add items:
    - ...
    
    * Updated items:
    - FirstFragment
    
    * Removed:
    ...

--- app/src/main/java/udemy/java/organizze/config/ConfigurationFirebase.java
+++ app/src/main/java/udemy/java/organizze/config/ConfigurationFirebase.java
@@ -26,4 +26,7 @@ public class ConfigurationFirebase {
         return userAuthentication;
 
     }
+
+
+
 }
--- app/src/main/java/udemy/java/organizze/fragment/FirstFragment.java
+++ app/src/main/java/udemy/java/organizze/fragment/FirstFragment.java
@@ -10,21 +10,36 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
+import java.text.DecimalFormat;
+
+import udemy.java.organizze.config.ConfigurationFirebase;
 import udemy.java.organizze.databinding.FragmentFirstBinding;
+import udemy.java.organizze.helper.Base64Custom;
+import udemy.java.organizze.model.User;
 
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
 
+    private final DatabaseReference firebaseRef = ConfigurationFirebase.getDatabaseReference();
+    private final FirebaseAuth userAuthentication = ConfigurationFirebase.getUserAuthentication();
+
     private MaterialCalendarView calendarRevenue;
 
     private TextView valueUserName, valueBalance;
-
+    private Double totalExpense = 0.0;
+    private Double totalRevenue = 0.0;
+    private Double userBalance  = 0.0;
 
     @Override
     public View onCreateView(
@@ -45,7 +60,44 @@ public class FirstFragment extends Fragment {
         valueBalance = binding.textViewRevenueBalance;
         calendarRevenue = binding.calendarViewRevenueCalender;
 
+
+    }
+
+    @Override
+    public void onResume() {
         configCalendarRevenue();
+        retrieveBalance();
+        super.onResume();
+    }
+
+    private void retrieveBalance() {
+        String userEmail = userAuthentication.getCurrentUser().getEmail();
+        String idUser = Base64Custom.encryptionBase64(userEmail);
+        DatabaseReference userRef = firebaseRef.child("users").child(idUser);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                User user = snapshot.getValue(User.class);
+                if (user != null) {
+                    totalExpense = user.getExpenseTotal();
+                    totalRevenue = user.getRevenueTotal();
+                }
+                userBalance = totalRevenue - totalExpense;
+
+                DecimalFormat decimalFormat = new DecimalFormat("0.##");
+                String resultFormat = decimalFormat.format(userBalance);
+
+                valueUserName.setText("Olá " + user.getName());
+                valueBalance.setText( resultFormat + " €");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
